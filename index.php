d1cb17b4e49d249a770e86dd9caf80d104f68012
Adrien Humilière <adhumi@gmail.com>
2012-08-05 04:31:02 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>CV Adrien Humilière</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+		<link rel="stylesheet" href="css/bootstrap-responsive.min.css" type="text/css">
+	</head>
+	<body style="padding:10px 0;">
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<div class="hero-unit">
+						<h1>Adrien Humilière</h1>
+						<p>Développeur web & iOS</p>
+					</div>
+				</div>
+			</div>
+		
+			<div class="row">
+				<div class="span10 offset1">
+					<p class="lead">
+						Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+					</p>
+				
+					<div class="page-header">
+						<h1>Expérience professionelle</h1>
+					</div>
+					
+					<div class="row">
+						<div class="span2">
+							<em>De septembre 2011 à août 2012</em>
+						</div>
+						<div class="span8">
+							<h3>Fantasy.fr - développeur web - Paris</h3>
+							<ul>
+								<li>Développement objet PHP5 via framework CakePHP.</li>
+								<li>Conception de base de données MySQL.</li>
+								<li>Intégration XHTML 1.0 Strict & CSS 3.</li>
+								<li>Analyse orientée ergonomie et SEO.</li>
+								<li>Architecture n-tiers et Design Patterns.</li>
+							</ul>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="span2">
+							<em>D'avril à août 2011</em>
+						</div>
+						<div class="span8">
+							<h3>Laboratoire IDES - Développeur logiciel - Orsay (91)</h3>
+							<p>Développement Fortran et optimisation sur un logiciel utilisé en géologie structurale.<br/>
+							Méthode des éléments finis, Math Kernel Library.</p>
+						</div>
+					</div>
+					
+					
+					<div class="page-header">
+						<h1>Compétences</h1>
+					</div>
+					
+					<div class="row">
+						<div class="span4 offset1">
+							<h3>Développement logiciel</h3>
+							<table class="table">
+								<tbody>
+									<tr>
+										<td>Objective-C</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:#CCC">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+									<tr>
+										<td>Java</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:rgb(147,160,47)">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+									<tr>
+										<td>C</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:#CCC">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+									<tr>
+										<td>Fortran</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:#CCC">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<div class="span4 offset1">
+							<h3>Développement web</h3>
+							<table class="table">
+								<tbody>
+									<tr>
+										<td>PHP</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:rgb(147,160,47)">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+									<tr>
+										<td>SQL</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:rgb(147,160,47)">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+									<tr>
+										<td>HTML</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:rgb(147,160,47)">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+									<tr>
+										<td>CSS</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:rgb(147,160,47)">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+									<tr>
+										<td>Javascript</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:#CCC">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					<div class="row">
+						<div class="span4 offset1">
+							<h3>Divers</h3>
+							<table class="table">
+								<tbody>
+									<tr>
+										<td>UML</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:#CCC">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+									<tr>
+										<td>Webdesign</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:rgb(147,160,47)">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<div class="span4 offset1">
+							<h3>Langues</h3>
+							<table class="table">
+								<tbody>
+									<tr>
+										<td>Anglais</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:rgb(157,177,24)">     </span><span class="label" style="background-color:rgb(147,160,47)">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+									<tr>
+										<td>Allemand</td>
+										<td>
+											<span class="label" style="background-color:rgb(177,199,33)">     </span><span class="label" style="background-color:#CCC">     </span><span class="label" style="background-color:#CCC">     </span><span class="label" style="background-color:#CCC">     </span>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					
+					<div class="page-header">
+						<h1>Projets</h1>
+					</div>
+					<div class="row">
+						<div class="span9 offset1">
+							<ul class="thumbnails">
+								<li class="span3">
+									<div class="thumbnail">
+										<img src="http://www.adhumi.fr/img/img-freepodapp.png" alt="Freepod pour iOS">
+										<div class="caption">
+											<h5>Freepod pour iOS</h5>
+											<p>Développement d'une application pour, notamment, la lecture de podcasts en streaming.<br/>Disponible sur l'<a href="">App Store</a>.</p>
+										</div>
+									</div>
+								</li>
+								<li class="span3">
+									<div class="thumbnail">
+										<img src="http://www.adhumi.fr/img/img-aphp.png" alt="Fiches travaux">
+										<div class="caption">
+											<h5>Fiches notification travaux</h5>
+											<p>Mise au point d'un système automatisé (PHP) de transmission de notifications travaux pour l'AP-HP.</p>
+										</div>
+									</div>
+								</li>
+								<li class="span3">
+									<div class="thumbnail">
+										<img src="http://www.adhumi.fr/img/img-tweetwall.png" alt="Tweetwall">
+										<div class="caption">
+											<h5>Tweetwall</h5>
+											<p>Projet personnel 100% HTML/JS. Affiche les avatars des personnes ayant tweeté un mot-clé donné.<br/> Disponible sur <a href="http://www.adhumi.fr/tw">adhumi.fr/tw</a>.</p>
+										</div>
+									</div>
+								</li>
+								<li class="span3">
+									<div class="thumbnail">
+										<img src="http://www.adhumi.fr/img/img-poligeek.png" alt="Poli*Geek">
+										<div class="caption">
+											<h5>Poli*Geek</h5>
+											<p>Site web (SPIP) pour le podcast <a href="http://www.poligeek.fr">Poli*Geek.</a></p>
+										</div>
+									</div>
+								</li>
+								<li class="span3">
+									<div class="thumbnail">
+										<img src="http://www.adhumi.fr/img/img-freepod.png" alt="Freepod">
+										<div class="caption">
+											<h5>Tweetwall</h5>
+											<p>Développement d'un site web (SPIP) pour le réseau de podcasts <a href="http://www.freepod.net">Freepod</a>.</p>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					
+					<div class="page-header">
+						<h1>Formation</h1>
+					</div>
+					
+					<div class="row">
+						<div class="span2">
+							<em>De 2009 à 2012</em>
+						</div>
+						<div class="span8">
+							<h3>Université Pierre et Marie Curie</h3>
+							<h4>Licence d'informatique, parcours Développeur d'applications, nouvelles technologies</h4>
+							<ul>
+								<li>Génie logiciel</li>
+								<li>Bases de données</li>
+								<li>Langage C</li>
+								<li>Java avancé</li>
+								<li>Programmation répartie</li>
+								<li>Administration système</li>
+								<li>Réseaux</li>
+								<li>Développement web</li>
+								<li>Méthodes de projet</li>
+							</ul>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="span2">
+							<em>De 2005 à 2009</em>
+						</div>
+						<div class="span8">
+							<h3>Université Pierre et Marie Curie</h3>
+							<h4>Sciences de la Terre</h4>
+							<br/>
+						</div>
+					</div>
+					
+					<div class="page-header">
+						<h1>Centres d'intérêt</h1>
+					</div>
+					
+					<div class="row">
+						<div class="span6 offset1">
+							<p>Programmation.</p>
+							<p>Pratique du VTT et du ski nordique, y compris en compétition.</p>
+							<p>Pratique occasionnelle de la randonnée en haute montagne et de l’alpinisme.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+			<hr/>
+			<footer class="footer">
+        		<a href="http://www.adhumi.fr">adhumi.fr</a>
+        	</footer>
+		</div>
+
+		
+		
+	</body>
+</html>
